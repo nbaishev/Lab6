eea760d4209f40fa96c3542dcd1cb97a31df990a
Николай Баишев <baishev.nk@stud.s-vfu.ru>
2022-01-09 17:19:38 +0900

    Add project files.

---
+++ Problem6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string[] words = s.Split(' ');
+            int max_len = 0;
+            string max_word = "";
+
+            foreach (string word in words)
+            {
+                if (word.Length > max_len)
+                {
+                    max_len = word.Length;
+                    max_word = word;
+                }
+            }
+            Console.WriteLine(max_word);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task6_2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            s = s.ToLower();
+            bool fl = true;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == ' ')
+                {
+                    s = s.Remove(i, 1);
+                    i--;
+                }
+            }
+
+            for (int i = 0; i < s.Length / 2; i++)
+            {
+                if (s[i] != s[s.Length - i - 1])
+                {
+                    fl = false;
+                    break;
+                }
+            }
+
+            if (fl)
+            {
+                Console.WriteLine("Палиндром");
+            }
+            else
+            {
+                Console.WriteLine("Не палиндром");
+            }
+            Console.ReadKey();
+        }
+    }
+}
